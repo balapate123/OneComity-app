9222e0ca9c2c9c9bb946c4c3b25ee7f712bb93b7
Bhargav <balapate1234@gmail.com>
2025-05-19 01:42:21 -0400

    Added Rate Limiter in Backend

--- onecomity-backend/app.js
+++ onecomity-backend/app.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
+
+
 
 app.use(cors());
 app.use(express.json());
+app.use(helmet());
+app.use(rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100
+}));
+
 
 app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/api/user', require('./routes/userRoutes'));
