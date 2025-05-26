a64c2309ae14381cad0eb3092539d7984c5d3273
Bhargav <balapate1234@gmail.com>
2025-05-25 21:24:28 -0400

    Added Expo Project Id in config

--- onecomity-frontend/app.config.js
+++ onecomity-frontend/app.config.js
@@ -4,6 +4,9 @@ export default ({ config }) => ({
   extra: {
     API_URL: process.env.API_URL,
     SOCKET_URL: process.env.SOCKET_URL,
+    "eas": {
+        "projectId": "2ae390c9-2b39-498b-8dbf-936307624a94"
+      }
     // Add more if needed
   },
 });
