c92dc0a15e98e1fec0bb12e5a180c2cc6b1200d9
Bhargav <balapate1234@gmail.com>
2025-05-26 02:00:06 -0400

    Added Logo and This is a Working APK Build

--- onecomity-frontend/app.config.js
+++ onecomity-frontend/app.config.js
@@ -1,12 +1,26 @@
-// app.config.js
 export default ({ config }) => ({
   ...config,
+  icon: "./assets/logo.png",
+  plugins: [
+    ...(config.plugins || []),
+    [
+      "expo-build-properties",
+      {
+        android: {
+          usesCleartextTraffic: true,
+        },
+      },
+    ],
+  ],
+  android: {
+    ...(config.android || {}),
+    usesCleartextTraffic: true, // (for good measure, but plugin is key!)
+  },
   extra: {
     API_URL: process.env.API_URL,
     SOCKET_URL: process.env.SOCKET_URL,
-    "eas": {
-        "projectId": "2ae390c9-2b39-498b-8dbf-936307624a94"
-      }
-    // Add more if needed
+    eas: {
+      projectId: "2ae390c9-2b39-498b-8dbf-936307624a94",
+    },
   },
 });
